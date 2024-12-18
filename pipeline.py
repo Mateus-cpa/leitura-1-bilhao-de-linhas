692e95001784da7a9d33809ad4772c8961d93932
Mateus Pinheiro <mateus.mcpa@gmail.com>
2024-12-18 07:24:49 -0300

    :construction: feat: criando os módulos python e old_python e comparação de tempo

--- pipeline.py
+++ pipeline.py
@@ -1,7 +1,8 @@
-from src.using_python_old import processar_temperaturas
+import src.using_python_old as old_python
+import src.using_python as new_python
+
 
 path = "data/measurements.txt"
-processar_temperaturas(path_do_txt = path)
 
-
-print('pronto!')
+dados_op, tempo_op = old_python.processar_temperaturas(path_do_txt = path)
+dados_np, tempo_np = new_python.processar_temperaturas(path_do_txt = path)
