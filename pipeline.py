57ca6f3bb53e2f0f7fa31102cfbc3bdbc6467b0b
Mateus Pinheiro <mateus.mcpa@gmail.com>
2024-12-20 07:34:37 -0300

    :sparkles: feat: incluindo análise do polars na análise

--- pipeline.py
+++ pipeline.py
@@ -2,6 +2,7 @@
 import src.using_python_old as old_python
 import src.using_python as new_python
 from src.using_pandas import create_df_with_pandas as pandas
+from src.using_polars import create_polars_df as polars
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,7 +10,7 @@ import matplotlib.pyplot as plt
 
 #create_files.main()
 
-lista_num_linhas = [1000,10000,100000,1000000]#,10000000,100000000] #adicionar Bilhão após testes ,1000000000]
+lista_num_linhas = [10000,100000,1000000]#,10000000,100000000] #adicionar Bilhão após testes ,1000000000]
 
 df_tempos_de_execucao = pd.DataFrame(columns=['num_linhas', 'metodo', 'tempo (s)'])
 
@@ -22,6 +23,7 @@ for num_linhas in lista_num_linhas:
     dados_op, tempo_op = old_python.processar_temperaturas(path_do_txt=path)
     dados_np, tempo_np = new_python.processar_temperaturas(path_do_txt=path, num_linhas=num_linhas)
     df_final, tempo_pd = pandas(path_do_txt=path, num_linhas=num_linhas)
+    df_polars, tempo_pl = polars(path_do_txt=path, num_linhas=num_linhas)
 
 
     # Cria um novo DataFrame com os resultados da iteração e concatena com o DataFrame principal
@@ -29,10 +31,11 @@ for num_linhas in lista_num_linhas:
     dict_to_append.append({'num_linhas': num_linhas, 'metodo': 'método antigo python', 'tempo (s)': tempo_op})
     dict_to_append.append({'num_linhas': num_linhas, 'metodo': 'método novo python', 'tempo (s)': tempo_np})
     dict_to_append.append({'num_linhas': num_linhas, 'metodo': 'pandas', 'tempo (s)': tempo_pd})
+    dict_to_append.append({'num_linhas': num_linhas, 'metodo': 'polars', 'tempo (s)': tempo_pl})
 
     df_to_append = pd.DataFrame(dict_to_append) #transforma dicionário em dataframe
     df_tempos_de_execucao = pd.concat([df_tempos_de_execucao,df_to_append])
-
+    df_tempos_de_execucao.reset_index()
 
 print(df_tempos_de_execucao)
 
@@ -45,6 +48,8 @@ fig, ax = plt.subplots()
 # Plota cada linha:
 for name, group in grouped:
     ax.plot(group['num_linhas'], group['tempo (s)'], label=name)
+    for x, y in zip(group['num_linhas'], group['tempo (s)']):
+        ax.annotate(f"{y:.3f}", (x, y), textcoords="offset points", xytext=(0,10), ha='center')
 
 # Configurando o gráfico
 ax.set_xlabel('Número de Linhas', fontsize=14)
@@ -52,6 +57,7 @@ ax.set_ylabel('Tempo de Execução (segundos)', fontsize=14)
 ax.set_title('Comparação de Desempenho', fontweight='bold')
 ax.grid(True, linestyle='--', alpha=0.7)
 ax.legend(loc='lower right')
-ax.set_yscale('log')  # Escala logarítmica no eixo y
+ax.set_yscale('log') # Escala logarítmica no eixo y 
+ax.set_xscale('log') # Escala logarítmica no eixo x
 plt.savefig('results/gráfico_comparativo.png', dpi=300)
 plt.show()
