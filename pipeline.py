85d7bd0e08423c41b4568dca32db03fe75aa32c9
Mateus Pinheiro <mateus.mcpa@gmail.com>
2024-12-21 06:54:45 -0300

    :sparkles: feat: incluindo duckdb no pipeline.

--- pipeline.py
+++ pipeline.py
@@ -3,6 +3,7 @@ import src.using_python_old as old_python
 import src.using_python as new_python
 from src.using_pandas import create_df_with_pandas as pandas
 from src.using_polars import create_polars_df as polars
+from src.using_duckdb import create_duckdb as duck
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -24,6 +25,7 @@ for num_linhas in lista_num_linhas:
     dados_np, tempo_np = new_python.processar_temperaturas(path_do_txt=path, num_linhas=num_linhas)
     df_final, tempo_pd = pandas(path_do_txt=path, num_linhas=num_linhas)
     df_polars, tempo_pl = polars(path_do_txt=path, num_linhas=num_linhas)
+    db_duck, tempo_dk = duck(path_do_txt=path, num_linhas=num_linhas)
 
 
     # Cria um novo DataFrame com os resultados da iteração e concatena com o DataFrame principal
@@ -32,6 +34,7 @@ for num_linhas in lista_num_linhas:
     dict_to_append.append({'num_linhas': num_linhas, 'metodo': 'método novo python', 'tempo (s)': tempo_np})
     dict_to_append.append({'num_linhas': num_linhas, 'metodo': 'pandas', 'tempo (s)': tempo_pd})
     dict_to_append.append({'num_linhas': num_linhas, 'metodo': 'polars', 'tempo (s)': tempo_pl})
+    dict_to_append.append({'num_linhas': num_linhas, 'metodo': 'duckdb', 'tempo (s)': tempo_dk})
 
     df_to_append = pd.DataFrame(dict_to_append) #transforma dicionário em dataframe
     df_tempos_de_execucao = pd.concat([df_tempos_de_execucao,df_to_append])
