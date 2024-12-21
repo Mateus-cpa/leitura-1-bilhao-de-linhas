e0f1b41807055ddfc0d945f78b030c0330218f31
Mateus Pinheiro <mateus.mcpa@gmail.com>
2024-12-21 13:48:22 -0300

    :broom: cleanup: alterando as mensagens de saída e reorganizando os comandos

--- src/using_pandas.py
+++ src/using_pandas.py
@@ -12,40 +12,18 @@ path_do_txt = "data/measurements_1000.txt"  # Certifique-se de que este é o cam
     return aggregated"""
 
 def create_df_with_pandas(path_do_txt, num_linhas, chunksize=10_000):
+    
+    import time
+    print(f"Iniciando o processamento do arquivo em PANDAS do arquivo com {num_linhas} linhas.")
+    start_time = time.time()
+
     import pandas as pd
- 
     #Constantes
     #chunksize = 100_000_000  # Define o tamanho de cada parte da subdivisão do dataframe
     #CONCURRENCY = cpu_count()
 
-    import time
-
-    print("Iniciando o processamento do arquivo.")
-    start_time = time.time()
-
-    """    total_chunks = num_linhas // chunksize + (1 if num_linhas % chunksize else 0)
-    results = []
-
-    # Regula Barra de progresso
-    if total_chunks > num_linhas:
-        total_bar_progress = num_linhas
-    else:
-        total_bar_progress = total_chunks
-
-    with pd.read_csv(path_do_txt, sep=';', header=None, names=['station', 'measure'], chunksize=chunksize) as reader:        
-        # Envolvendo o iterador com tqdm para visualizar o progresso
-        with Pool(CONCURRENCY) as pool:
-            for chunk in tqdm(reader, total=total_chunks, desc="Processando"):
-                # Processa cada chunk em paralelo
-                result = pool.apply_async(process_chunk, (chunk,))
-                results.append(result)
     
-
-
-            results = [result.get() for result in results]
-
-    final_df = pd.concat(results, ignore_index=True)"""
-    print(f'Processando arquivo com {num_linhas} linhas...')
+    print(f'Processando arquivo PANDAS com {num_linhas} linhas...')
     final_dfs = []
     for chunk in pd.read_csv(path_do_txt, sep=';', header=None, names=['station', 'measure'], chunksize=chunksize):
         final_df = chunk.groupby('station').agg(['min','max','mean']).reset_index().sort_values('station')
@@ -55,7 +33,7 @@ def create_df_with_pandas(path_do_txt, num_linhas, chunksize=10_000):
 
     time_elapsed = time.time() - start_time
 
-    print(f"Tempo de processamento em pandas durou: {time_elapsed:.2f} segundos")
+    print(f"Processamento no módulo PANDAS concluído em {time_elapsed:.3f} segundos.\n")
 
     return final_aggregated_df, time_elapsed
 
